9a406e14150ef7a911b07e1589f90d50c7808288
André <andre.dunkel@t-online.de>
2025-04-17 18:23:13 +0200

    some ui-stuff

--- source/MovieApp/Client/Program.cs
+++ source/MovieApp/Client/Program.cs
@@ -7,5 +7,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string graphqlServerPath = builder.HostEnvironment.BaseAddress + "graphql";
+builder.Services.AddMovieClient()
+	.ConfigureHttpClient(client =>
+	{
+		client.BaseAddress = new Uri(graphqlServerPath);
+	});
 
 await builder.Build().RunAsync();
---
+++ source/MovieApp/Client/Shared/NavMenuBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MovieApp.Client.Shared
+{
+	/// <ChangeLog>
+	/// <Create Datum="17.04.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	public class NavMenuBase : ComponentBase
+	{
+	}
+}
