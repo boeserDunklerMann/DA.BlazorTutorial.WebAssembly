30e153fa59dd9d8d2aabf04760fe13b83307eeb4
André <andre.dunkel@t-online.de>
2025-04-17 11:05:23 +0200

    IMovie added

---
+++ src/MovieApp/Server/Interfaces/IMovie.cs
@@ -0,0 +1,12 @@
+﻿using MovieApp.Server.Models;
+
+namespace MovieApp.Server.Interfaces
+{
+	/// <ChangeLog>
+	/// <Create Datum="17.04.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	public interface IMovie
+	{
+		Task<List<Genre>> GetGenre();
+	}
+}
