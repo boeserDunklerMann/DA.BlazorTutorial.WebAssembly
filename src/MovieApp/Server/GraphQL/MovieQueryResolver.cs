09a32c2ada845e6e3fce6bfeb4e7014587bcde41
André <andre.dunkel@t-online.de>
2025-04-17 11:16:02 +0200

    MovieQueryResolver added

---
+++ src/MovieApp/Server/GraphQL/MovieQueryResolver.cs
@@ -0,0 +1,24 @@
+﻿using MovieApp.Server.Interfaces;
+using MovieApp.Server.Models;
+
+namespace MovieApp.Server.GraphQL
+{
+	/// <ChangeLog>
+	/// <Create Datum="17.04.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	public class MovieQueryResolver
+	{
+		private readonly IMovie _movieService;
+
+		public MovieQueryResolver(IMovie movieService)
+		{
+			_movieService = movieService;
+		}
+
+		[GraphQLDescription("Gets the list of genres")]
+		public async Task<List<Genre>> GetGenreList()
+		{
+			return await _movieService.GetGenre();
+		}
+	}
+}
