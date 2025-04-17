4cb2d0e0f895f2f5eede52816fc0bf961285e56a
André <andre.dunkel@t-online.de>
2025-04-17 12:05:42 +0200

    DataAnnotations added in Movie class

--- src/MovieApp/Shared/Models/Movie.cs
+++ src/MovieApp/Shared/Models/Movie.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieApp.Server.Models;
-
+/// <ChangeLog>
+/// <Create Datum="17.04.2025" Entwickler="DA" />
+/// </ChangeLog>
 public partial class Movie
 {
     public int MovieId { get; set; }
 
+    [Required]
     public string Title { get; set; } = null!;
-
+    [Required]
     public string Overview { get; set; } = null!;
-
+    [Required]
     public string Genre { get; set; } = null!;
-
+    [Required]
     public string Language { get; set; } = null!;
-
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "This field accepts positive numbers only.")]
     public int Duration { get; set; }
-
+    [Required]
+    [Range(0, 10.0, ErrorMessage = "The value should be les than or equal to 10.")]
     public decimal? Rating { get; set; }
 
     public string? PosterPath { get; set; }
+
+    public Movie()
+    {
+        Title = string.Empty;
+        Overview = string.Empty;
+        Genre = string.Empty;
+        Language = string.Empty;
+    }
 }
