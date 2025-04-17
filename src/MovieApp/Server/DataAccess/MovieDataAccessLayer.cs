75b9f733e452e96194b99c4819eb02c726377ba4
André <andre.dunkel@t-online.de>
2025-04-17 11:11:12 +0200

    dataaccessLayer added

---
+++ src/MovieApp/Server/DataAccess/MovieDataAccessLayer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Server.Interfaces;
+using MovieApp.Server.Models;
+
+namespace MovieApp.Server.DataAccess
+{
+	/// <ChangeLog>
+	/// <Create Datum="17.04.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	public class MovieDataAccessLayer : IMovie
+	{
+		private readonly MovieDbContext _dbContext;
+		public MovieDataAccessLayer(IDbContextFactory<MovieDbContext> dbContext)
+		{
+			_dbContext = dbContext.CreateDbContext();
+		}
+
+		public async Task<List<Genre>> GetGenre()
+		{
+			return await _dbContext.Genres.AsNoTracking().ToListAsync();
+		}
+	}
+}
