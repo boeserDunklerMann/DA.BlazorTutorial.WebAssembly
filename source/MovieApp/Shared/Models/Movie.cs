b48833809a14440d4c05030c4d05fa7afe70a4ae
André <andre.dunkel@t-online.de>
2025-04-17 17:15:26 +0200

    added attributes tro Movie's  props

--- source/MovieApp/Shared/Models/Movie.cs
+++ source/MovieApp/Shared/Models/Movie.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieApp.Server.Models
 {
     public partial class Movie
     {
         public int MovieId { get; set; }
+        [Required]
         public string Title { get; set; } = null!;
-        public string Overview { get; set; } = null!;
-        public string Genre { get; set; } = null!;
-        public string Language { get; set; } = null!;
-        public int Duration { get; set; }
-        public decimal? Rating { get; set; }
+		[Required]
+		public string Overview { get; set; } = null!;
+		[Required]
+		public string Genre { get; set; } = null!;
+		[Required]
+		public string Language { get; set; } = null!;
+		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = " This field accepts only positive numbers.")]
+		public int Duration { get; set; }
+		[Required]
+		[Range(0, 10.0, ErrorMessage = "The value should be less than or equal to 10.")]
+		public decimal? Rating { get; set; }
         public string? PosterPath { get; set; }
     }
 }
