df74207198c0e53d0df53938092d0db26fff5d05
André <andre.dunkel@t-online.de>
2025-04-17 14:06:03 +0200

    AddEditMovie.razor added

---
+++ src/MovieApp/Client/Pages/AddEditMovie.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using MovieApp.Client.GraphQLAPIClient;
+using MovieApp.Server.Models;
+
+namespace MovieApp.Client.Pages
+{
+	/// <ChangeLog>
+	/// <Create Datum="17.04.2025" Entwickler="DA" />
+	/// </ChangeLog>
+	public partial class AddEditMovie : ComponentBase
+	{
+		[Inject]
+		public NavigationManager NavigationManager { get; set; } = default!;
+		[Inject]
+		MovieClient MovieClient { get; set; } = default!;
+
+		[Parameter]
+		public int MovieID { get; set; }
+
+		protected string Title = "Add";
+		public Movie movie = new();
+		protected List<Genre>? lstGenre = new();
+		protected string? imagePeview;
+		const int MaxFileSize = 10 * 1024 * 1024;   // 10 MB
+		const string DefaultStatus = "Maximum file size allowed for the image is 10 MB";
+		protected string status = DefaultStatus;
+
+	}
+}
