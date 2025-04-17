61f8528114347f89a911ed28062c44d3970066eb
André <andre.dunkel@t-online.de>
2025-04-17 11:02:45 +0200

    models scaffolded from db

---
+++ src/MovieApp/Server/Models/MovieDbContext.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieApp.Server.Models;
+
+public partial class MovieDbContext : DbContext
+{    public MovieDbContext(DbContextOptions<MovieDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    public virtual DbSet<UserMaster> UserMasters { get; set; }
+
+    public virtual DbSet<UserType> UserTypes { get; set; }
+
+    public virtual DbSet<Watchlist> Watchlists { get; set; }
+
+    public virtual DbSet<WatchlistItem> WatchlistItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>(entity =>
+        {
+            entity.HasKey(e => e.GenreId).HasName("PK__Genre__0385055E53D097FE");
+
+            entity.ToTable("Genre");
+
+            entity.Property(e => e.GenreId).HasColumnName("GenreID");
+            entity.Property(e => e.GenreName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasKey(e => e.MovieId).HasName("PK__Movie__4BD2943A840D7E69");
+
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.MovieId).HasColumnName("MovieID");
+            entity.Property(e => e.Genre)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Language)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Overview)
+                .HasMaxLength(1024)
+                .IsUnicode(false);
+            entity.Property(e => e.PosterPath)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Rating).HasColumnType("decimal(2, 1)");
+            entity.Property(e => e.Title)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<UserMaster>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__UserMast__1788CCACBF857492");
+
+            entity.ToTable("UserMaster");
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(6)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.UserTypeName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<UserType>(entity =>
+        {
+            entity.HasKey(e => e.UserTypeId).HasName("PK__UserType__40D2D8F6BEEA5522");
+
+            entity.ToTable("UserType");
+
+            entity.Property(e => e.UserTypeId).HasColumnName("UserTypeID");
+            entity.Property(e => e.UserTypeName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Watchlist>(entity =>
+        {
+            entity.HasKey(e => e.WatchlistId).HasName("PK__Watchlis__48DEAA2B0BA45022");
+
+            entity.ToTable("Watchlist");
+
+            entity.Property(e => e.WatchlistId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("WatchlistID");
+            entity.Property(e => e.DateCreated).HasColumnType("datetime");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+        });
+
+        modelBuilder.Entity<WatchlistItem>(entity =>
+        {
+            entity.HasKey(e => e.WatchlistItemId).HasName("PK__Watchlis__E97BA0ADB4287768");
+
+            entity.Property(e => e.WatchlistItemId).HasColumnName("WatchlistItemID");
+            entity.Property(e => e.MovieId).HasColumnName("MovieID");
+            entity.Property(e => e.WatchlistId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("WatchlistID");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- src/MovieApp/Server/Program.cs
+++ src/MovieApp/Server/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Configuration.AddJsonFile("appsettings.local.json");
 
 var app = builder.Build();
 
