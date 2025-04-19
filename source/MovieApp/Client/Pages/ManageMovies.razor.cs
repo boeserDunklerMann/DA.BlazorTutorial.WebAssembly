1bcdd8f1a9632d4fedaff68d7a6ffb1bd71752e4
André <andre.dunkel@t-online.de>
2025-04-19 15:22:01 +0200

    Admin Panel added

---
+++ source/MovieApp/Client/Pages/ManageMovies.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using MovieApp.Client.GraphQLAPIClient;
+using MovieApp.Server.Models;
+
+namespace MovieApp.Client.Pages
+{
+	/// <ChangeLog>
+		/// <Create Datum="19.04.2025" Entwickler="DA" />
+		/// </ChangeLog>
+	public class ManageMoviesBase: ComponentBase
+	{
+		[Inject]
+		private MovieClient MovieClient { get; set; } = default!;
+		[Inject]
+		private NavigationManager NavigationManager { get; set; } = default!;
+		
+		protected List<Movie>? lstMovie = new();
+		protected Movie? movie = new();
+
+		protected override async Task OnInitializedAsync()
+		{
+			await GetMovieList();
+		}
+
+		protected async Task GetMovieList()
+		{
+			var results = await MovieClient.FetchMovieList.ExecuteAsync();
+
+			lstMovie = results?.Data?.MovieList.Select(x => new Movie
+			{
+				MovieId = x.MovieId,
+				Title = x.Title,
+				Duration = x.Duration,
+				Genre = x.Genre,
+				Language = x.Language,
+				PosterPath = x.PosterPath,
+				Rating = x.Rating,
+			}).ToList();
+		}
+
+		protected void DeleteConfirm(int movieID)
+		{
+			movie = lstMovie?.FirstOrDefault(m => m.MovieId == movieID);
+		}
+
+		protected async Task DeleteMovie(int movieID)
+		{
+			var response = await MovieClient.DeleteMovieData.ExecuteAsync(movieID);
+			if (response.Data is not null)
+			{
+				await GetMovieList();
+			}
+		}
+	}
+}
